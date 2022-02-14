d12f2bc1e28148fdc0e775e8f386e01064199541
Mauaqp <peraltilla93@hotmail.com>
2022-02-14 12:14:41 -0500

    primer commit

---
+++ Hola.java
@@ -0,0 +1,11 @@
+public class Hola {
+    public static void main(String args[]){
+        int edad = 28;
+        String nombre = "Mauricio Peraltilla";
+        String ciudad = "Arequipa";
+        
+        System.out.println("Mi nombre es "+ nombre);
+        System.out.println("Tengo " + edad + " años");
+        System.out.println("Mi ciudad natal es " + ciudad);
+    }
+}
